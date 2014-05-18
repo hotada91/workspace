a328774e93c0a4eecfdbd85b2dff42f4b428988c
dac <hotada91@gmail.com>
2014-05-18 08:33:02 +0700

    Revert "start"
    This reverts commit 2345651fa163ecc1a9ab9212492f3ea71b2e2082.

--- HosiptalSizeCardApp/src/CardForm/MainClass.java
+++
@@ -1,8 +0,0 @@
-package CardForm;
-public class MainClass {
-	public static void main(String[] args){
-		//PrintForm form = new PrintForm();	
-		
-		ApplicationForm appForm = new ApplicationForm();
-	}
-}
